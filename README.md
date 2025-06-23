4bb3a409c72144333bee2dc3a524f92b8e47fe08
Gabriel Brasil <gabrielfmbrasil03@gmail.com>
2025-06-22 22:56:24 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Controles:
+
+WASD: Movimentação
+
+Mouse: Olhar ao redor
+
+Scroll do mouse: Zoom
+
+Teclas 1, 2, 3: Ligar/desligar luzes (1: Key light; 2: Fill light; 3: Back light)
+
+ESC: Sair
---
+++ build/CMakeFiles/GB.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GB.dir/common/glad.c.obj"
+  "CMakeFiles/GB.dir/common/glad.c.obj.d"
+  "CMakeFiles/GB.dir/src/GB.cpp.obj"
+  "CMakeFiles/GB.dir/src/GB.cpp.obj.d"
+  "GB.exe"
+  "GB.exe.manifest"
+  "GB.pdb"
+  "libGB.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/GB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
